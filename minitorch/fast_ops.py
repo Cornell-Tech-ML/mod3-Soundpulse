47e8096774c71d958c1a5986ad85a648fc6272d7
Toby Io <toby.io@outlook.com>
2024-11-21 12:58:42 -0500

    fix: map reduce

--- minitorch/fast_ops.py
+++ minitorch/fast_ops.py
@@ -231,25 +231,17 @@ def tensor_zip(
             for i in prange(len(out)):
                 out[i] = fn(float(a_storage[i]), float(b_storage[i]))
         else:
-            out_index: Index = np.zeros(MAX_DIMS, np.int32)
-            a_index: Index = np.zeros(MAX_DIMS, np.int32)
-            b_index: Index = np.zeros(MAX_DIMS, np.int32)
-            out_positions = np.zeros(len(out), np.int32)
-            a_positions = np.zeros(len(out), np.int32)
-            b_positions = np.zeros(len(out), np.int32)
-
-            for i in range(len(out)):
-                to_index(i, out_shape, out_index)
-                broadcast_index(out_index, out_shape, a_shape, a_index)
-                broadcast_index(out_index, out_shape, b_shape, b_index)
-                out_positions[i] = index_to_position(out_index, out_strides)
-                a_positions[i] = index_to_position(a_index, a_strides)
-                b_positions[i] = index_to_position(b_index, b_strides)
-
             for i in prange(len(out)):
-                out[out_positions[i]] = fn(
-                    float(a_storage[a_positions[i]]), float(b_storage[b_positions[i]])
-                )
+                out_index: Index = np.zeros(MAX_DIMS, np.int32)
+                a_index: Index = np.zeros(MAX_DIMS, np.int32)
+                b_index: Index = np.zeros(MAX_DIMS, np.int32)
+                to_index(i, out_shape, out_index)
+                o = index_to_position(out_index, out_strides)
+                broadcast_index(out_index, out_shape, a_shape, a_index)
+                j = index_to_position(a_index, a_strides)
+                broadcast_index(out_index, out_shape, b_shape, b_index)
+                k = index_to_position(b_index, b_strides)
+                out[o] = fn(a_storage[j], b_storage[k])
 
     return njit(_zip, parallel=True)  # type: ignore
 
